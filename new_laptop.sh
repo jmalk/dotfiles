a03b458ff856f2c2a8e42364d387b21c118fc9dc
Josh Malkinson <josh.malkinson@lush.co.uk>
2023-01-03 17:22:15 +0000

    Automate more of setup

---
+++ new_laptop.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Install apps
+brew install gh
+brew install glab
+brew install z
+brew install neovim
+brew install the_silver_searcher
+brew install tree
+brew install markdown
+brew install --cask authy
+brew install --cask spotify
+brew install --cask firefox
+
+# Install VimPlug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Symlink dotfiles
+# TODO: Could replace this with version controlling entire home directory
+ln -s ~/dotfiles/sharedrc ~/.sharedrc
+ln -s ~/dotfiles/bashrc ~/.bashrc
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/vimrc ~/.vimrc
+mkdir -p ~/.config/nvim
+ln -s ~/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
